5bc0a0a46cf4141955647e6c719cdb41e7c52c58
santiagoA26 <santiagoaa260705@gmail.com>
2026-05-14 17:25:14 -0500

    configurar CORS y endpoints para la conexión externa y lograr mostrar el frontend.

--- LastStarShoesAPI/Program.cs
+++ LastStarShoesAPI/Program.cs
@@ -5,35 +5,51 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+
+// 1. Configuración de cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddScoped<IClienteService, ClienteService>();
 builder.Services.AddScoped<IProductoService, ProductoService>();
 builder.Services.AddScoped<IAdministradorService, AdministradorService>();
 builder.Services.AddScoped<ICompraService, CompraService>();
-// 2. Modificamos esta línea para ignorar los ciclos infinitos en el JSON
+
+// 2. Controladores con manejo de ciclos (ACTUALIZADO)
 builder.Services.AddControllers()
     .AddJsonOptions(options => {
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 3. Conexión a Base de Datos
 builder.Services.AddDbContext<LaststarShoesDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+
 
 app.UseAuthorization();
 
